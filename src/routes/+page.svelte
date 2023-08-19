1b00a2145abd673bd340753cf9c06c037549df85
LempekPL <37885098+LempekPL@users.noreply.github.com>
2023-08-19 04:12:35 +0200

    testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,28 +5,42 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Lempek</title>
+	<meta name="title" content="Lempek" />
+	<meta name="description" content="Learn everything about Lempek." />
+	<meta name="theme-color" content="#69E621">
+
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://lempek.dev/" />
+	<meta property="og:title" content="Lempek" />
+	<meta property="og:description" content="Learn about more about Lempek." />
+	<meta property="og:image" content="$lib/LEMPEKbanner.png" />
+
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://metatags.io/" />
+	<meta property="twitter:title" content="Lempek" />
+	<meta property="twitter:description" content="Learn about more about Lempek." />
+	<meta property="twitter:image" content="$lib/images/LEMPEKbanner.png" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<!--<section>-->
+<!--	<h1>-->
+<!--		<span class="welcome">-->
+<!--			<picture>-->
+<!--				<source srcset={welcome} type="image/webp" />-->
+<!--				<img src={welcome_fallback} alt="Welcome" />-->
+<!--			</picture>-->
+<!--		</span>-->
 
-		to your new<br />SvelteKit app
-	</h1>
+<!--		to your new<br />SvelteKit app-->
+<!--	</h1>-->
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+<!--	<h2>-->
+<!--		try editing <strong>src/routes/+page.svelte</strong>-->
+<!--	</h2>-->
 
-	<Counter />
-</section>
+<!--	<Counter />-->
+<!--</section>-->
 
 <style>
 	section {
