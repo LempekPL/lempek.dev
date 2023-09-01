c8fbbf1442c378b1f3a202e3c0fee055db507803
LempekPL <37885098+LempekPL@users.noreply.github.com>
2023-09-01 20:45:36 +0200

    better transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,14 @@
     import ShortBio from "./ShortBio.svelte";
     import Navigation from "./Navigation.svelte";
     import storeTheme from "$lib/store-theme";
+    import {fly} from 'svelte/transition';
+
+    export let data;
 </script>
 
 <svelte:head>
     <meta name="color-scheme" content={$storeTheme === "system" ? "light dark" : $storeTheme}/>
-    <link rel="stylesheet" href={`/theme/${$storeTheme}.css`} />
+    <link rel="stylesheet" href={`/theme/${$storeTheme}.css`}/>
 </svelte:head>
 
 <div class="app">
@@ -18,25 +21,31 @@
     <Navigation/>
 
     <main>
-        <slot/>
+        {#key data.url}
+            <div in:fly={{ x: -200, duration: 300, delay: 300 }}
+                 out:fly={{ x: 200, duration: 300 }}>
+                <slot/>
+            </div>
+        {/key}
     </main>
 </div>
 
 <style lang="scss">
-    .app {
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-        background-color: var(--background);
-    }
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    background-color: var(--background);
+    overflow: clip;
+  }
 
-    main {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        max-width: 64rem;
-        margin: 0 auto;
-        box-sizing: border-box;
-    }
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 64rem;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,18 +1,50 @@
 <script lang="ts">
     import {t} from "svelte-i18n";
     import {page} from "$app/stores";
+    import {afterUpdate} from "svelte";
+    import {tweened} from 'svelte/motion';
+    import {writable} from "svelte/store";
 
-    $: path = $page.url.pathname.slice($page.url.pathname.lastIndexOf('/'))
+    const left = tweened(0, {duration: 250});
+    const width = tweened(0, {duration: 100});
+    const cX = writable(0);
+    const cY = writable(0);
+
+    let home: HTMLAnchorElement;
+    let about: HTMLAnchorElement;
+    let projects: HTMLAnchorElement;
+
+    $: path = $page.url.pathname.slice($page.url.pathname.lastIndexOf('/'));
+
+    afterUpdate(() => {
+        if (path === "/") {
+            left.set(home.offsetLeft);
+            width.set(home.offsetWidth);
+        } else if (path === "/about") {
+            left.set(about.offsetLeft);
+            width.set(about.offsetWidth);
+        } else if (path === "/projects") {
+            left.set(projects.offsetLeft);
+            width.set(projects.offsetWidth);
+        }
+    })
+
+    function moveClicker(e: MouseEvent & {currentTarget: (EventTarget & HTMLAnchorElement)}) {
+        cX.set(e.offsetX-16);
+        cY.set(e.offsetY-16);
+    }
 </script>
-
 <nav>
-    <a href="/" class:selected={"/"===path}>{$t("nav.home")}</a>
-    <a href="/about" class:selected={"/about"===path}>{$t("nav.about")}</a>
-    <a href="/projects" class:selected={"/projects"===path}>{$t("nav.projects")}</a>
+    <div class="selector" style="left: {$left}px; width: {$width}px;"/>
+    <!--    class:selected={"/"===path}-->
+    <a href="/" bind:this={home} data-sveltekit-noscroll>{$t("nav.home")}</a>
+    <a href="/about" bind:this={about} data-sveltekit-noscroll>{$t("nav.about")}</a>
+    <a href="/projects" bind:this={projects} data-sveltekit-noscroll>{$t("nav.projects")}</a>
 </nav>
 
 <style lang="scss">
   nav {
+    z-index: 1;
     width: fit-content;
     margin: 1rem auto;
     border-radius: 1000rem;
@@ -24,9 +56,21 @@
     border: .25rem solid var(--header);
     box-shadow: black 0 4px 8px -4px;
     transition: 200ms;
+    position: sticky;
+    top: 1rem;
 
     &:hover {
       box-shadow: black 0 4px 8px -2px;
+    }
+
+    .selector {
+      position: absolute;
+      background: var(--navbar-button);
+      top: 0;
+      left: 0;
+      bottom: 0;
+      border-radius: 1000rem;
+      z-index: -1;
     }
 
     a {
@@ -34,11 +78,32 @@
       text-decoration: none;
       padding: .75rem 1rem;
       border-radius: 1000rem;
+      position: relative;
+      overflow: hidden;
 
+      &:after {
+        content: "";
+        transition: 200ms;
+        z-index: -2;
+      }
+      &:hover:after {
+        content: "";
+        transition: 200ms;
+        position: absolute;
+        background: #bebebe;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      }
     }
   }
 
-  .selected {
-    background: var(--navbar-button);
-  }
+  //.selector {
+  //
+  //}
+
+  //.selected {
+  //  background: var(--navbar-button);
+  //}
 </style>
